24266342ff5822a64420bf7ca0da215300d9e89c
Ihyatt <inas.raheema@gmail.com>
2023-06-10 23:06:08 -0700

    centered column

--- lib/main.dart
+++ lib/main.dart
@@ -15,86 +15,79 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            children: <Widget> [
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('images/luna.png'),
               ),
-              Text(
-                'Inas Hyatt',
-                style: TextStyle(
-                  fontFamily: 'TiltPrism',
-                  fontSize: 40,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                )
-              ),
-              Text(
-                  'SOFTWARE ENGINEER',
+              Text('Inas Hyatt',
                   style: TextStyle(
-                    fontFamily: 'SourceSans3 Light',
-                    fontSize: 20,
-                    color: Colors.teal[100],
-                    letterSpacing: 2.5,
+                    fontFamily: 'TiltPrism',
+                    fontSize: 40,
+                    color: Colors.white,
                     fontWeight: FontWeight.bold,
-                  )
+                  )),
+              Text(
+                'SOFTWARE ENGINEER',
+                style: TextStyle(
+                  fontFamily: 'SourceSans3 Light',
+                  fontSize: 20,
+                  color: Colors.teal[100],
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
+              SizedBox(
+                height: 20.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(
                   vertical: 10.0,
                   horizontal: 25.0,
                 ),
-                child: Row(
-                  children: <Widget> [
-                    Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width:10.0
-                    ),
-                    Text(
-                        '510.555.5555',
-                        style: TextStyle(
-                          fontFamily: 'SourceSans3 Light',
-                          fontSize: 20,
-                          color: Colors.teal,
-                          letterSpacing: 2.5,
-                        )
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: Row(
-                  children: <Widget> [
-                    Icon(
-                      Icons.email,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '510.555.5555',
+                    style: TextStyle(
+                      fontFamily: 'SourceSans3 Light',
+                      fontSize: 20,
                       color: Colors.teal,
+                      letterSpacing: 2.5,
                     ),
-                    SizedBox(
-                        width:10.0
-                    ),
-                    Text(
-                        'inas.hyatt@email.com',
-                        style: TextStyle(
-                          fontFamily: 'SourceSans3 Light',
-                          fontSize: 20,
-                          color: Colors.teal,
-                          letterSpacing: 2.5,
-                        )
-                    )
-                  ],
+                  ),
                 ),
-             ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'inas.hyatt@email.com',
+                    style: TextStyle(
+                      fontFamily: 'SourceSans3 Light',
+                      fontSize: 20,
+                      color: Colors.teal,
+                      letterSpacing: 2.5,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
