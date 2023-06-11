2a959efdeef43a62e6771b16269a28bd1efd1de3
Ihyatt <inas.raheema@gmail.com>
2023-06-10 21:35:01 -0700

    completed card, added fonts and icons, also cented UI

--- lib/main.dart
+++ lib/main.dart
@@ -39,8 +39,64 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   )
               ),
-            ]
-          )
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: Row(
+                  children: <Widget> [
+                    Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width:10.0
+                    ),
+                    Text(
+                        '510.555.5555',
+                        style: TextStyle(
+                          fontFamily: 'SourceSans3 Light',
+                          fontSize: 20,
+                          color: Colors.teal,
+                          letterSpacing: 2.5,
+                        )
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: Row(
+                  children: <Widget> [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                        width:10.0
+                    ),
+                    Text(
+                        'inas.hyatt@email.com',
+                        style: TextStyle(
+                          fontFamily: 'SourceSans3 Light',
+                          fontSize: 20,
+                          color: Colors.teal,
+                          letterSpacing: 2.5,
+                        )
+                    )
+                  ],
+                ),
+             ),
+            ],
+          ),
         ),
       ),
     );
